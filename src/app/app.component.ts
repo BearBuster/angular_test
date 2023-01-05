e8954d50ea6a1d621ad3cddb5167f8b59af6fe1e
BearBuster <50841034+BearBuster@users.noreply.github.com>
2023-01-05 14:20:27 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+export interface FirstTaskResult{
+  arrayIndex: number,
+  calculationMax: number
+}
+
+export interface Client{
+  idBank: number,
+  idClient: number,
+  totalSum: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'task_5';
+
+  private _firstTaskResult: FirstTaskResult
+  private _secondTaskResult: Array<Client>
+
+
+  get secondTaskResult(): Array<Client> {
+    return this._secondTaskResult;
+  }
+
+  set secondTaskResult(value: Array<Client>) {
+    this._secondTaskResult = value;
+  }
+
+  set firstTaskResult(value: FirstTaskResult) {
+    this._firstTaskResult = value;
+  }
+
+
+  get firstTaskResult(): FirstTaskResult {
+    return this._firstTaskResult;
+  }
+
+  setFirstTask(firstTaskResult: FirstTaskResult){
+    this._secondTaskResult = []
+    this._firstTaskResult = firstTaskResult
+  }
+
+  setSecondTask(secondTaskResult: Array<Client>){
+    this._firstTaskResult = null
+    this._secondTaskResult = secondTaskResult
+
+    console.log(this._firstTaskResult);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './components/input-form/input-form.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
+import {FormsModule} from "@angular/forms";
+import { ClientCardComponent } from './components/client-card/client-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
+    ResultListComponent,
+    ClientCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
